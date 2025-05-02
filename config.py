ea8b56f95eb7321fd079eb1741749bfdb297a459
pietrō <pmarini72107@gmail.com>
2025-05-02 21:09:21 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,77 +1,113 @@
-from typing import Dict
+"""
+This module contains the configuration settings for the API server, including:
+- Authentication server settings
+- Log server settings
+- API server settings
+- Database settings
+- HTTP status codes and their explanations
+- Authorization settings
+"""
 
-# Define authentication server data
+from typing import Dict
+from datetime import timedelta
+
+# Authentication server related settings
 AUTH_SERVER_HOST: str = "localhost"  # The host of the authentication server
-AUTH_SERVER_PORT: int = 5002  # The port of the authentication server
-AUTH_SERVER_VALIDATE_URL: str = (
-    f"http://{AUTH_SERVER_HOST}:{AUTH_SERVER_PORT}/auth/validate"  # The URL to validate a token
-)
+AUTH_SERVER_PORT: int = 6002  # The port of the authentication server
 AUTH_SERVER_NAME_IN_LOG: str = "auth-server"
 AUTH_SERVER_DEBUG_MODE: bool = True
-JWT_TOKEN_DURATION: int = 3  # In hours
-JWT_SECRET_KEY: str = "Lorem ipsum dolor sit amet eget."
+AUTH_SERVER_SSL: bool = False  # Whether the authentication server uses SSL/TLS or not
+AUTH_SERVER_SSL_CERT: str = (
+    "/path/to/cert.pem"  # The path to the SSL/TLS certificate file
+)
+AUTH_SERVER_SSL_KEY: str = "/path/to/key.pem"  # The path to the SSL/TLS key file
 
-# Define log server host, port and server name in log files
+# Log server related settings
 LOG_SERVER_HOST: str = "localhost"  # The host of the log server
 LOG_SERVER_PORT: int = (
-    514  # The port of the log server (default syslog port, can modified to open port for testing)
+    6014  # The port of the log server (default syslog port, can modified to open port for testing)
 )
 LOG_FILE_NAME: str = "pctowa_log.txt"
 LOGGER_NAME: str = "pctowa_logger"  # The name of the logger
 LOG_SERVER_NAME_IN_LOG: str = "log-server"  # The name of the server in the log messages
-RATE_LIMIT = 100  # Maximum messages per source
-TIME_WINDOW = 1  # Time window in seconds
-DELAYED_LOGS_QUEUE_SIZE = 100  # The size of the delayed logs queue (if the queue is full, the oldest logs will be removed to make space for new ones)
-
-# Define host and port of the API server
-API_SERVER_HOST: str = (
-    "172.16.1.98"  # The host of the API server (should be only server open to the rest of the network)
-)
-API_SERVER_PORT: int = 5000  # The port of the API server
-API_SERVER_NAME_IN_LOG: str = "api-server"  # The name of the server in the log messages
-API_VERSION: str = "v1"  # The version of the API
-URL_PREFIX: str = f"/api/{API_VERSION}/"
-API_SERVER_DEBUG_MODE: bool = True
-
-# Database configuration
-DB_HOST: str = "localhost"
-DB_NAME: str = "pctowa"
-DB_USER: str = "pctowa"
-DB_PASSWORD: str = "pctowa2025"
-CONNECTION_POOL_SIZE: int = 20  # The maximum number of connections in the pool
-
-# HTTP status codes and their explanations
-STATUS_CODES_EXPLANATIONS: Dict[int, str] = {
-    200: "OK - The request has succeeded.",
-    201: "Created - The request has been fulfilled and resulted in a new resource being created.",
-    202: "Accepted - The request has been accepted for processing, but the processing has not been completed.",
-    204: "No Content - The server successfully processed the request, but is not returning any content.",
-    400: "Bad Request - The server could not understand the request due to invalid syntax.",
-    401: "Unauthorized - The client must authenticate itself to get the requested response.",
-    403: "Forbidden - The client does not have access rights to the content.",
-    404: "Not Found - The server can not find the requested resource.",
-    405: "Method Not Allowed - The request method is known by the server but is not supported by the target resource.",
-    409: "Conflict - The request could not be completed due to a conflict with the current state of the resource.",
-    500: "Internal Server Error - The server has encountered a situation it doesn't know how to handle.",
-    502: "Bad Gateway - The server was acting as a gateway or proxy and received an invalid response from the upstream server.",
-    503: "Service Unavailable - The server is not ready to handle the request.",
+RATE_LIMIT_AMOUNT: int = 100  # Maximum messages per source
+RATE_LIMIT_AMOUNT_TIME_WINDOW: int = 1  # Time window in seconds
+DELAYED_LOGS_QUEUE_SIZE: int = 100  # The size of the delayed logs queue
+# (if the queue is full, the oldest logs will
+#  be removed to make space for new ones)
+SYSLOG_SEVERITY_MAP: Dict[str, int] = {  # Define a severity map for the syslog server
+    "emergency": 0,  # System is unusable
+    "alert": 1,  # Action must be taken immediately
+    "critical": 2,  # Critical conditions
+    "error": 3,  # Error conditions
+    "warning": 4,  # Warning conditions
+    "notice": 5,  # Normal but significant condition
+    "info": 6,  # Informational messages
+    "debug": 7,  # Debug-level messages
 }
 
+# API server related settings
+# | API server settings
+API_SERVER_HOST: str = "localhost"
+API_SERVER_PORT: int = 6000
+API_SERVER_NAME_IN_LOG: str = "api-server"  # The name of the server in the log messages
+API_VERSION: str = "v1"  # The version of the API
+URL_PREFIX: str = f"/api/{API_VERSION}/"  # The prefix for all API endpoints
+API_SERVER_DEBUG_MODE: bool = True  # Whether the API server is in debug mode or not
+API_SERVER_SSL: bool = False  # Whether the API server uses SSL/TLS or not
+API_SERVER_SSL_CERT: str = (
+    "/path/to/cert.pem"  # The path to the SSL/TLS certificate file
+)
+API_SERVER_SSL_KEY: str = "/path/to/key.pem"  # The path to the SSL/TLS key file
+
+# JWT custom configuration
+JWT_SECRET_KEY: str = "Lorem ipsum dolor sit amet eget."
+JWT_ALGORITHM: str = "HS256"  # The algorithm used to sign the JWT token
+JWT_QUERY_STRING_NAME = "jwt_token"  # Custom name for the query string parameter
+JWT_ACCESS_COOKIE_NAME = "jwt_access_cookie"  # Custom name for the access token cookie
+JWT_REFRESH_COOKIE_NAME = (
+    "jwt_refresh_cookie"  # Custom name for the refresh token cookie
+)
+JWT_JSON_KEY = "jwt_token"  # Custom key for the access token in JSON payloads
+JWT_REFRESH_JSON_KEY = (
+    "jwt_refresh_token"  # Custom key for the refresh token in JSON payloads
+)
+JWT_TOKEN_LOCATION = [
+    "headers",
+    "cookies",
+    "query_string",
+    "json",
+]  # Where to look for the JWT token
+JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=10)  # Refresh token valid duration
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=3)  # Access token valid duration
+# | Database configuration
+DB_HOST: str = "localhost"
+DB_NAME: str = "idranjia"
+DB_USER: str = "root"
+DB_PASSWORD: str = ""
+CONNECTION_POOL_SIZE: int = 20  # The maximum number of connections in the pool
+
+
+# Miscellaneous settings
+# | HTTP status codes
 STATUS_CODES: Dict[str, int] = {
     "not_found": 404,
     "unauthorized": 401,
     "forbidden": 403,
     "conflict": 409,
+    "precondition_failed": 412,
+    "unprocessable_entity": 422,
     "bad_request": 400,
     "created": 201,
     "ok": 200,
     "no_content": 204,
     "internal_error": 500,
+    "service_unavailable": 503,
 }
+# | Roles and their corresponding IDs
+ROLES: Dict[int, str] = {0: "admin", 1: "teacher", 2: "tutor", 3: "supertutor"}
 
-# Authorization related
-ROLES = {0: "admin", 1: "teacher", 2: "tutor", 3: "supertutor"}
-
+# | Standard not authorized message
 NOT_AUTHORIZED_MESSAGE: Dict[str, str] = {
     "outcome": "error, action not permitted with current user"
 }
